f430e511f7f0e427394dd1f04e9f4c01643c3685
alexis-gauluet <alexis.gauluet@edu.esiee.fr>
2024-12-06 13:46:37 +0000

    Quelques itterations sur des listes de listes contenues d'int

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-#### Imports et définition des variables globales
-import random
+"""bienvenue, joyeux hunger games"""
+import csv
 
 FILENAME = "listes.csv"
 
@@ -15,38 +15,51 @@ def read_data(filename):
         list: le contenu du fichier (1 list par ligne)
     """
     l = []
+    with open(filename, 'r',encoding='utf8') as f:
+        r = csv.reader(f,delimiter=';')
+        l=[[int(x) for x in line] for line in r]
     return l
 
 def get_list_k(data, k):
-    l = []
-    return l
+    """retourne la ligne d'indice k"""
+    return data[k]
 
 def get_first(l):
-    return None
+    """retourne le premier élément de la liste"""
+    return int(l[0])
 
 def get_last(l):
-    return None
+    """retourne le dernier element de la liste"""
+    return int(l[-1])
 
 def get_max(l):
-    return None
+    """retourne l'element maximum de la liste"""
+    return int(max(l))
 
 def get_min(l):
-    return None
+    """retourne l'élément minimum de la liste"""
+    return int(min(l))
 
 def get_sum(l):
-    return None
-
+    """retourne la somme de tous les éléments de la liste"""
+    s=0
+    i=0
+    while i<len(l) :
+        s+=int(l[i])
+        i+=1
+    return s
 
 #### Fonction principale
 
 
 def main():
-    pass
-    # data = read_data(FILENAME)
-    # for i, l in enumerate(data):
+    """fonction principale"""
+    data = read_data(FILENAME)
+    print (data)
+    #for i, l in enumerate(data):
     #     print(i, l)
-    # k = 37
-    # print(k, get_list_k(data, 37))
+    # k = -1
+    # print (get_list_k(data,k))
 
 
 if __name__ == "__main__":
